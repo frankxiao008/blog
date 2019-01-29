c97c5c8126f97bf5448995422489c50f4f9dffb9
frank <xiaosaihong2008@gmail.com>
2019-01-28 19:24:56 -0500

    get the port right

--- app.js
+++ app.js
@@ -71,9 +71,10 @@ app.use(function(err, req, res, next) {
 });
 
 let server = http.createServer(app);
-let PORT = process.env.PORT || cfg.port;
+//let PORT = process.env.PORT || cfg.port;
 
-server.listen(PORT, ()=>{
-    console.log(`Server started at port: ${PORT}`);
-});
+// server.listen(PORT, ()=>{
+//     console.log(`Server started at port: ${PORT}`);
+// });
 
+server.listen(process.env.PORT || 3000 );
